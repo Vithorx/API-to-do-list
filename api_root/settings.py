4baf98e33f64baf9aa9e12fe2e68ebed0870a4de
Vithorx <vithorsousa777@gmail.com>
2025-06-26 14:49:06 -0300

    Atualizações

--- api_root/settings.py
+++ api_root/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '81771dcb9a9a6cea96fc857d7640b605')
+SECRET_KEY = os.environ.get('SECRET_KEY', '8021ea844941737a687edc1c353a9c2c')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() = 'true'
