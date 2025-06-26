e03a944636e24815460639cddea626922a296abc
Vithorx <vithorsousa777@gmail.com>
2025-06-26 16:09:16 -0300

    Subindo modificações em settings.py

--- api_root/settings.py
+++ api_root/settings.py
@@ -138,6 +138,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_URL = os.path.join(BASE_DIR,'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
