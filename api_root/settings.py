0d488190bfa9b877783dc4535cd94d30abd31ec2
Vithorx <vithorsousa777@gmail.com>
2025-06-27 12:22:53 -0300

    Correções de urls em cors

--- api_root/settings.py
+++ api_root/settings.py
@@ -158,7 +158,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', 'https://to-dolista.netlify.app,http://127.0.0.1:5500').split(',')
+CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', 'http://127.0.0.1:5500,http://localhost:3000').split(',')
 
 
 SIMPLE_JWT = {
